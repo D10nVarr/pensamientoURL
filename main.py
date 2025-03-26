c6d1818bcb7eaaa30908a3d0e7cde735a9026794
D10nVarr <d1390xd.v@gmail.com>
2025-03-26 11:33:43 -0600

    Ejercicio clase 26/03/25

---
+++ main.py
@@ -0,0 +1,39 @@
+## Ejercicio 1 
+#for i in range(1,11):
+#    if i%2==1:
+#        print(i)
+
+#Ejercicio 2
+#x = 1
+
+#while x<11:
+#    if x%2==1:
+#        print(x)
+#    x+=1
+
+
+#Ejercicio 3
+
+#while True:
+#    palabra=input("Ingrese una palabra para salir del bucle: ").lower()
+    
+#    if palabra=="chupacabra":
+#        print("¡Has dejado el bucle con éxito!")
+#        break
+    
+#Ejercicio 4
+
+#user_word=input("Ingrese su palabra: ").upper()
+
+#vocal="A,E,I,O,U"
+
+#for i in user_word:
+#    if i in vocal:
+#       continue
+#    print (i)
+
+
+    
+    
+
+
